550086acc4f1b7e1c51bf0127852c0fd8b8abe9b
Matheus <matheus.frndes@gmail.com>
2014-12-10 15:44:02 -0200

    Modularized truncate

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+int truncate(int, int);
+
 int main() {
     FILE *infile, *outfile;
     char fileType[10], *in, *out, *image;
@@ -9,8 +11,8 @@ int main() {
     unsigned char r, g, b;
     int nR, nG, nB, brightness, contrast;
 
-    brightness = 50;
-    contrast = 3;
+    brightness = 0;
+    contrast = 4;
 
     infile = fopen("\\\\psf\\Home\\Desktop\\UFSCar.pnm", "rb");
     outfile = fopen("\\\\psf\\Home\\Desktop\\UFSCargain.pnm", "wb");
@@ -24,7 +26,7 @@ int main() {
 
     pixels = width * height;
 
-    in = out = image = (char *)malloc(3 * pixels);
+    in = out = image = (char *) malloc(3 * pixels);
 
     fread(image, 3, pixels, infile);
 
@@ -34,65 +36,22 @@ int main() {
         b = *in++;
 
         __asm {
-            movzx eax, r // r += brightness amount
-            add eax, brightness
+            movzx eax, r
+            movzx ebx, g
+            movzx ecx, b
 
-            movzx ebx, g // g += brightness amount
-            add ebx, brightness
-
-            cmp eax, 0 // if red < 0
-            jg truncate_small_r
-
-            red_big:
-                cmp eax, depth // if reg > depth
-                jl truncate_big_r
-                jmp green
-
-            truncate_small_r: // if red < 0
-                mov eax, 0 // then red = 0
-                jmp red_big
-
-            truncate_big_r: // if red > depth
-                mov eax, depth // then red = depth
-
-            green:
-            cmp ebx, 0 // if green < 0
-            jl truncate_small_g
-
-            green_big:
-                cmp ebx, depth // if green > depth
-                jg truncate_big_g
-                jmp blue
-
-            truncate_small_g: // if green < 0
-                mov ebx, 0 // then green = 0
-                jmp green_big
-
-            truncate_big_g: // if green > depth
-                mov ebx, depth // then green = depth
-
-            blue:
-            cmp ecx, 0 // if blue < 0
-            jl truncate_small_b
-
-            blue_big:
-                cmp ecx, depth // if blue > depth
-                jg  truncate_big_b
-                jmp next_
-
-            truncate_small_b: // if blue < 0
-                mov ecx, 0 // then blue = 0
-                jmp blue_big
-
-            truncate_big_b: // if green > depth
-                mov ecx, depth // then green = depth
-
-            next_:
+            add eax, brightness // r += brightness amount
+            add ebx, brightness // g += brightness amount
+            add ecx, brightness// b += brightness amount
 
             mov nR, eax
             mov nG, ebx
             mov nB, ecx
         }
+
+        nR = truncate(depth, nR);
+        nG = truncate(depth, nG);
+        nB = truncate(depth, nB);
 
         *out++ = nR;
         *out++ = nG;
@@ -108,4 +67,25 @@ int main() {
 
 
     return 0;
+}
+
+int truncate(int threshold, int value) {
+    __asm{
+        mov eax, value
+        cmp eax, 0
+        jl truncate_small
+        cmp eax, threshold
+        jg truncate_big
+        jmp endd
+
+        truncate_small:
+            mov eax, 0
+            jmp endd
+        truncate_big:
+            mov eax, threshold
+
+        endd:
+            mov value, eax
+    }
+    return value;
 }
