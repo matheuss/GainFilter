2603cf376435aafdd084289baa10dd1855fbb0a7
Matheus <denis_cappelini@hotmail.com>
2014-12-14 21:45:20 -0200

    Brightness fully working on SSE/SSE2

--- parallel.cpp
+++ parallel.cpp
@@ -7,7 +7,7 @@ int main() {
     FILE *infile, *outfile;
     char fileType[10], *in, *out, *image;
     int width, height, depth, pixels;
-    unsigned char r, g, b;
+    unsigned char r, g, b, red[16], green[16], blue[16], brightness[16], contrast[16], base[16];
     int nR, nG, nB;
 
     clock_t start, end;
@@ -38,52 +38,43 @@ int main() {
         image[(3*pixels) + i] = 0; // fill extra pixels with zeros
     }
 
-    unsigned char red[16], green[16], blue[16], brightness[16], contrast[16], base[16];
-
     for (int i = 0; i < 16; i++) {
         brightness[i] = 50;
         contrast[i] = 4;
-        red[i] = (unsigned char) *in++;
-        green[i] = (unsigned char) *in++;
-        blue[i] = (unsigned char) *in++;
         base[i] = depth;
     }
 
-    __asm {
-        movups xmm0, red
-        movups xmm1, green
-        movups xmm2, blue
-        movups xmm3, brightness
-        movups xmm4, base
-        paddb xmm0, xmm3
-        paddb xmm1, xmm3
-        paddb xmm2, xmm3
-        pcmpgtw xmm0, xmm4
-        pcmpgtw xmm1, xmm4
-        //movups xmm0, a
-        //movups xmm1, b1
-        //movups xmm3, c
-        //paddsw xmm0, xmm1
-        //pcmpgtw xmm0, xmm3
-        //movups a, xmm0
-    }
-
     start = clock();
-    for (int i = 0; i < pixels; i++) {
-        r = *in++;
-        g = *in++;
-        b = *in++;
-
-        __asm {
-
-
+    for (int i = 0; i < (pixels + extra) / 16; i++) {
+        for (int j = 0; j < 16; j++) {
+            red[j] = (unsigned char) *in++;
+            green[j] = (unsigned char) *in++;
+            blue[j] = (unsigned char) *in++;
         }
 
-        //*out++ = nR;
-        //*out++ = nG;
-        //*out++ = nB;
+        __asm {
+            movups xmm0, red
+            movups xmm1, green
+            movups xmm2, blue
+            movups xmm3, brightness
+            movups xmm4, base
+            paddusb xmm0, xmm3 // red += brightness
+            paddusb xmm1, xmm3 // green += brightness
+            paddusb xmm2, xmm3 // blue += brightness
+
+            movups red, xmm0
+            movups green, xmm1
+            movups blue, xmm2
+        }
+
+        for (int j = 0; j < 16; j++) {
+            *out++ = red[j];
+            *out++ = green[j];
+            *out++ = blue[j];
+        }
     }
     end = clock();
+
 
     fwrite(image, 3, pixels, outfile);
 
